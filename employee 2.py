4b1ebf96b9623e35df696b2745e07e5ef62d55e5
PriyaPatni <40827329+PriyaPatni@users.noreply.github.com>
2019-03-19 08:15:03 +0530

    Update employee 2.py

--- employee 2.py
+++ employee 2.py
@@ -10,19 +10,24 @@ class employee:
              self.salary=s
          else:
             print("Invalid salary")
-            self.salary=0.0
             self.salary=input("enter again")
     def  show(self):
-        print("First Name= ",self.firstname)
-        print("Second Name= ", self.lastname)
-        print("Salary= ", self.salary)
+        print("First Name = ",self.firstname)
+        print("Second Name = ", self.lastname)
+        print("Salary = ", self.salary)
     def rais(self):
         self.salary=self.salary+self.salary/10
 e1=employee()
 e2=employee()
-e1.get("Priya","Patni",5000.00)
+n=input("enter first name for e1 ")
+l=input("enter last name for e1 ")
+s=int(input("enter salary for e1 "))
+e1.get(n,l,s)
+n=input("enter first name for e2 ")
+l=input("enter last name for e2 ")
+s=int(input("enter salary for e2 "))
+e2.get(n,l,s)
 e1.show()
-e2.get("xyz","abc",3000.00)
 e2.show()
 print("After 10% raise")
 e1.rais()
